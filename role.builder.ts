eda876dbeafea799cbcfe7b115b121227d8efa08
Olivier <zeachco@gmail.com>
2019-07-23 18:12:40 -0400

    added info about pending structures

--- role.builder.ts
+++ role.builder.ts
@@ -1,5 +1,5 @@
 import { ICreep } from './enums';
-import { harvestSourceBasedOfIndex } from './utils';
+import { harvestSourceBasedOfIndex, log } from './utils';
 
 export function builderAI(creep: ICreep, index: number) {
    if (creep.memory.building && creep.carry.energy == 0) {
@@ -13,10 +13,18 @@ export function builderAI(creep: ICreep, index: number) {
 
    if (creep.memory.building) {
       var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-      // console.log(targets);
-      if (targets.length) {
-         if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(targets[0], {
+
+      const bestTargets = targets.sort((a, b) => {
+         return 0;
+      });
+
+      if (bestTargets.length) {
+         log(
+            `${bestTargets.length} construction site(s) pending`,
+            bestTargets.map(s => s.structureType).join(', ')
+         );
+         if (creep.build(bestTargets[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(bestTargets[0], {
                visualizePathStyle: { stroke: '#ffffff' },
             });
          }
