409db5ede1fa242c569ac6ef2082da0e122c999c
Olivier <zeachco@gmail.com>
2019-08-04 00:09:27 -0400

    moved configs to config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,11 @@
+import { ROLE_UPGRADE } from './scripts/upgrade';
+import { ROLE_STORE } from './scripts/store';
+import { ROLE_BUILD } from './scripts/build';
+import { ROLE_REPAIR } from './scripts/repair';
+import { ROLE_HARVEST } from './scripts/harvest';
+import { ROLE_IDLE } from './scripts/idle';
+import { IRoleConfig, IRolesMap } from './types';
+
 type TPowerStructures = STRUCTURE_SPAWN | STRUCTURE_TOWER | STRUCTURE_EXTENSION;
 
 export const energyStructureByOrder: TPowerStructures[] = [
@@ -7,3 +15,34 @@ export const energyStructureByOrder: TPowerStructures[] = [
    // STRUCTURE_CONTAINER,
    // STRUCTURE_POWER_BANK,
 ];
+
+export const CREEPS_PER_TIERS = 10;
+export const MAX_CREEPS = 20;
+
+export const BODY_TIERS = [
+   [WORK, CARRY, MOVE],
+   [WORK, WORK, CARRY, MOVE],
+   [WORK, WORK, WORK, CARRY, MOVE],
+   [WORK, WORK, WORK, WORK, CARRY, MOVE],
+   [MOVE, WORK, WORK, WORK, CARRY, CARRY, MOVE],
+   [MOVE, MOVE, WORK, WORK, WORK, CARRY, CARRY, MOVE],
+   [MOVE, MOVE, WORK, WORK, WORK, CARRY, CARRY, ATTACK, MOVE],
+   [MOVE, MOVE, WORK, WORK, WORK, CARRY, CARRY, RANGED_ATTACK, MOVE],
+];
+
+export const rolesDispatch: IRoleConfig[] = [
+   ROLE_UPGRADE,
+   ROLE_STORE,
+   ROLE_BUILD,
+   ROLE_REPAIR,
+   ROLE_HARVEST,
+   ROLE_IDLE,
+];
+
+export const ROLES: IRolesMap = rolesDispatch.reduce(
+   (acc: IRolesMap, role: IRoleConfig) => {
+      acc[role.name] = role;
+      return acc;
+   },
+   {}
+);
--- src/inventory.ts
+++ src/inventory.ts
@@ -1,42 +1,13 @@
 import { log } from './utils';
-import { ICreep, ISpawn, IRoleConfig, IRolesMap, ICreepMemory } from './types';
-import { ROLE_HARVEST } from './scripts/harvest';
-import { ROLE_STORE } from './scripts/store';
-import { ROLE_BUILD } from './scripts/build';
-import { ROLE_UPGRADE } from './scripts/upgrade';
-import { ROLE_REPAIR } from './scripts/repair';
+import { ICreep, ISpawn, IRoleConfig, ICreepMemory } from './types';
 import { ROLE_IDLE } from './scripts/idle';
-
-const CREEPS_PER_TIERS = 10;
-const MAX_CREEPS = 20;
-
-const BODY_TIERS = [
-   [WORK, CARRY, MOVE],
-   [WORK, WORK, CARRY, MOVE],
-   [WORK, WORK, WORK, CARRY, MOVE],
-   [WORK, WORK, WORK, WORK, CARRY, MOVE],
-   [MOVE, WORK, WORK, WORK, CARRY, CARRY, MOVE],
-   [MOVE, MOVE, WORK, WORK, WORK, CARRY, CARRY, MOVE],
-   [MOVE, MOVE, WORK, WORK, WORK, CARRY, CARRY, ATTACK, MOVE],
-   [MOVE, MOVE, WORK, WORK, WORK, CARRY, CARRY, RANGED_ATTACK, MOVE],
-];
-
-const rolesDispatch: IRoleConfig[] = [
-   ROLE_UPGRADE,
-   ROLE_STORE,
-   ROLE_BUILD,
-   ROLE_REPAIR,
-   ROLE_HARVEST,
-   ROLE_IDLE,
-];
-
-const ROLES: IRolesMap = rolesDispatch.reduce(
-   (acc: IRolesMap, role: IRoleConfig) => {
-      acc[role.name] = role;
-      return acc;
-   },
-   {}
-);
+import {
+   rolesDispatch,
+   ROLES,
+   MAX_CREEPS,
+   BODY_TIERS,
+   CREEPS_PER_TIERS,
+} from './config';
 
 const findRole = (
    spawn: ISpawn,
