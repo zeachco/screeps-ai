cc76e9415780a2cd5f5834abe99ff97ce0bf76b3
Olivier <zeachco@gmail.com>
2019-08-29 13:11:18 -0400

    store targets

--- src/scripts/store.ts
+++ src/scripts/store.ts
@@ -2,13 +2,24 @@ import { ICreep, IRoleConfig } from '../types';
 import { countCreepsByRole, moveToOptions } from '../utils';
 
 export const run = (creep: ICreep) => {
-   const target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-      filter(s: StructureExtension) {
-         return (
-            s.isActive() && s.energyCapacity > 0 && s.energy < s.energyCapacity
-         );
-      },
-   });
+   const targets = creep.room
+      .find<StructureContainer>(FIND_MY_STRUCTURES, {
+         filter(s: StructureContainer) {
+            return s.storeCapacity > 0;
+         },
+      })
+      .sort((a, b) => b.store.energy - a.store.energy);
+   const target =
+      targets[0] ||
+      creep.pos.findClosestByPath(FIND_STRUCTURES, {
+         filter(s: StructureExtension) {
+            return (
+               s.isActive() &&
+               s.energyCapacity > 0 &&
+               s.energy < s.energyCapacity
+            );
+         },
+      });
 
    // TODO prioritize by ENERGY_STRUCT_BY_NEEDS
    // smaller inventory first
